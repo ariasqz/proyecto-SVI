d37401ade804fc6369a3a5ddf3d11b615a02109f
DavidMayorgaS08 <davidmayorga@colegioguanenta.edu.co>
2022-10-11 11:27:17 -0500

    Menú

--- js/custom.js
+++ js/custom.js
@@ -1,19 +1,4 @@
-$(document).ready(function(){
-    $('.up').hide();
-    $(window).scroll(function(){
-        if($(this).scrollTop() > 100){
-            $('.up').fadeIn('500');
-        }
-        else{
-            $('.up').fadeOut('1000');
-        }
-    });
-    $('.up').click(function(){
-        $('body, html').animate({
-            scrollTop:0
-        });
-    });
-});
+
 
 window.addEventListener("load", function(){
     this.document.getElementById("loader").classList.toggle("loader2")
